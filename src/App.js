29d193859769986fa03c8278d0ff8974790375a5
Deepbabaiadak37 <deekumaradak@gmail.com>
2023-12-29 13:44:03 +0530

    second

--- src/App.js
+++ src/App.js
@@ -11,53 +11,7 @@ import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <div className="App">
-      {/* <h1>Tum jo pucho Apni ehmiyat mujhse...
-        <p>ek tumse baat na karne se...</p>
-        <p>mera pura duniya udaas ho jata ...</p> </h1>
-
-        <div> 
-              I am patient with you , i am trying to understand you 
-              Though you show me that u dont care ,but I always feel u are with me ..
-              I am always with you .i want to support you at every situation and take care of you ..
-              ...but i always visualize a beautiful bond with you filled with love and friendship ,till my last breath .
-
-              cause only you can understand me , and i feel home talking with u . 
-              i find peace with you .May be i am not able to make you happy , thats why i always want you to communicate with me .
-              so that i will never dissapoint you and never make u feel lonely 
-              Specially at your worst i love u little extra 
-
-              My intention is simply to be with you forver and to love you ,I dont believe others 
-              first time i can believe in you 
-
-              You are such a kind hearted simply beautiful person from heart .
-              you are charismatic .I dont want anyone will play with your heart .
-              I will take care of your heart and we both will be fine with eachother .
-              You are my love + friendship both in 1 person .
-              I already have written you and me both name's first letter in my insta bio . I never ashamed to tell everyone whom i love the most 
-              but do you also wanna be with me Megha ? 
-
-              i will never gonna leave you or misunderstand you . if i will have some doubts i will direcly ask you or both we will join teams meet and solve our problems 
-              i am  a "Lets fix this person "
-              I am already yours. 
-              just need your hand to grow together . i will not hamper your personal space and your personal happiness.
-              just need a peacefull friendship + love bond with you where we both support each other ,and follow our dreams 
-              grow together , enjoy our own journey bt we will be assured that we are for each other we love each other and we have no jealousy 
-              no fear of loosing ...just a peacefull realtionship with so much care and love i want  with you 
-              its not the type of nibba nibbi love ..i want a mature frindship type of understanding love with you 
-              where we will never leave each other's back and will suppport at every situation
-              and finally we will marry each other 
-              thats all 
-
-
-              i told you all about whatever i had to tell 
-              I am waiting for you ..........................and for your answer ......
-
-
-
-
-        </div>
-        
-         */}
+      
 
 
 <Container>
